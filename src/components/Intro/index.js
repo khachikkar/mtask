ea99a0ba86de316246b6704fd4e7862464d41ca7
khachikkar <khachikkarapetyan78@gmail.com>
2024-12-15 02:16:13 +0400

    installed framer motion for animation

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import "./index.css"
+import intro from "../../core/images/intro.png"
+
+
 
 const Intro = () => {
   return (
     <div className="ree">
-      hello Mtask
+        <div className="left">
+
+            <h1>Mtask. Task Management App</h1>
+            <div className="glow-on-hover"> Go to my Space</div>
+
+        </div>
+
+
+      <div className="right">
+            <img src={intro} />
+      </div>
     </div>
   )
 }
